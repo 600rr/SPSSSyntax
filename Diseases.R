92a9d630a8d906beeb022865db71761153e8883a
DragonflyStats <kobriendublin@gmail.com>
2014-08-09 10:45:34 +0100

    Create Diseases.R

---
+++ Diseases.R
@@ -0,0 +1,16 @@
+###################################################################################
+###################################################################################
+
+ #  Move the following code to new source files.
+ #  
+
+
+
+
+
+# ICD classed ailments grouped into 16 groups.
+# Three main ailments: ICDcat1, ICDcat2, ICDcat3.
+# ICDcat1 should have about 1400 cases.
+# ICDcat2 and ICDcat3 will be nearly all null values.
+
+##########################

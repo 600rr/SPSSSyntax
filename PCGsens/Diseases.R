d8a6f4065d48519f39c1325c89a0ce71cc5351aa
DragonflyStats <kobriendublin@gmail.com>
2014-08-09 13:51:49 +0100

    Update and rename Diseases.R to PCGsens/Diseases.R

--- PCGsens/Diseases.R
+++ PCGsens/Diseases.R
@@ -1,16 +1,21 @@
 ###################################################################################
 ###################################################################################
 
- #  Move the following code to new source files.
- #  
-
-
-
-
 
 # ICD classed ailments grouped into 16 groups.
 # Three main ailments: ICDcat1, ICDcat2, ICDcat3.
 # ICDcat1 should have about 1400 cases.
 # ICDcat2 and ICDcat3 will be nearly all null values.
 
+# 1. 
+# 2. 
+# 3. 
+# 4. Skin Diseases
+# 5. 
+# 6. 
+# 7.
+# 8. 
+# 9.
+
 ##########################
+

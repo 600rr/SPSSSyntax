b56fa77b7bb3049110c82a60f4a92c9b966ad525
DragonflyStats <kobriendublin@gmail.com>
2014-08-09 11:39:57 +0100

    Create Habits-S24toS30.R

---
+++ Habits-S24toS30.R
@@ -0,0 +1,36 @@
+###########################
+
+# S24 Frequency of Alcohol Consumption
+
+# - If Never ( outcome 1) then go to S27 - S25 and S26 should be NULL
+# - If Infrequent (outcomes 2 or 3) then go to S26 -  S25 should be NULL
+
+##########################
+# S25 Level of Alchol Consumption
+
+# S25a - Beer/Cider
+# S25b - Wines Glasses
+# S25c - Spirits
+# S25d - Alcopops
+
+#########################
+
+# S26 - Occassions of Drinking in Excess of 6 units
+
+# S26a - Females only
+# S26b -
+# S26c - 
+# S26d - 
+# S26e -
+
+#########################
+
+# S29 How many Householders Smoke?
+# - Should have full response
+
+########################
+
+# S30 Controlled Substances
+# - Outcomes 1 to 3
+# - Should have full response
+

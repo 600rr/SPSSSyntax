5809ac2680382e290b3048b1b4280c7fb66d42c4
DragonflyStats <kobriendublin@gmail.com>
2014-08-11 13:16:32 -0700

    Update and rename PCGsens-variabsles.R to PCGsens/variables.R

---
+++ PCGsens/variables.R
@@ -0,0 +1,39 @@
+PCGsens.varlist.prelim <- c("X1", "X2", "bpc3AS1", "bpc3AS2", "bpc3AS3")
+PCGsens.vardesc.prelim <- c("Gender", "DOB", "ExitReason", "ExitMonth", "ExitYear")
+
+PCGsens.varlist.S1to15 <- c("bpc3S1", "bpc3S2", "bpc3S3", "bpc3S4", "bpc3S5", 
+"bpc3S6", "bpc3S7", "bpc3S8", "bpc3S9", "bpc3S10", "bpc3S11", "bpc3S12", "bpc3S13", "bpc3S14", "bpc3S15") 
+
+PCGsens.varlist.S16to26 <- c("bpc3S16", "bpc3S17", "bpc3S18a", "bpc3S18b", "bpc3S18c", "bpc3S19a", "bpc3S19b", "bpc3S19c", 
+"bpc3S20", "bpc3S21a", "bpc3S21b", "bpc3S21c", "bpc3S21d", "bpc3S21e", "bpc3S21f", "bpc3S21g", 
+"bpc3S21h", "bpc3S21i", "bpc3S22", "bpc3S23", "bpc3S24", "bpc3S25", "bpc3S26a", "bpc3S26b", "bpc3S26c", 
+"bpc3S26d", "bpc3S26f")
+
+
+###############################################################
+
+
+# S18report.R
+S18report=function(gr=1,hh=4)
+{
+S18names <- c("bpc3S21a", "bpc3S21b", "bpc3S21c")
+S18values <- c(TEMP$bpc3s18a,TEMP$bpc3s18b,TEMP$bpc3s18c)
+S18desc <- c("..")
+
+output=data.frame(S18names,S18values,S18desc)
+return(output)
+}
+
+
+###############################################################
+
+# S21report.R
+S21report=function(gr=1,hh=4)
+{
+S21names <- c("bpc3S21a", "bpc3S21b", "bpc3S21c", "bpc3S21d", "bpc3S21e", "bpc3S21f", "bpc3S21g")
+S21values <- c(TEMP$bpc3s21a,TEMP$bpc3s21b,TEMP$bpc3s21c,TEMP$bpc3s21d,TEMP$bpc3s21e,TEMP$bpc3s21f,TEMP$bpc3s21g)
+S21desc <- c("..")
+
+output=data.frame(S21names,S21values,S21desc)
+return(output)
+}

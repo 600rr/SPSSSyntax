074f43a168d3e4074df1db34b3686d47033156ef
DragonflyStats <kobriendublin@gmail.com>
2014-08-11 15:18:32 -0700

    Create LookUpTables.R

---
+++ LookUpTables.R
@@ -0,0 +1,7 @@
+
+RelationList =c("Spouse","Partner","Parent","Stepparent","..","..","Child","..","..","..","..")
+
+GenderList=c("Male","Female")
+
+SESList=c("..","..","..","..","..","..","..","..")
+

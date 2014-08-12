289cf176cbebe7879c4e4b19e8486e78970c3f47
DragonflyStats <kobriendublin@gmail.com>
2014-08-12 13:36:03 +0100

    Update LookUpTables.R

--- LookUpTables.R
+++ LookUpTables.R
@@ -1,7 +1,11 @@
 
-RelationList =c("Spouse","Partner","Parent","Stepparent","..","..","Child","..","..","..","..")
+RelationList =c("Spouse","Partner","Parent","Stepparent","..","..","Child","..","..","..","Sibling","Step-sibling",
+"..","..","..","Grandparent","..","..","..","Other")
+length(RelationList)
 
 GenderList=c("Male","Female")
 
 SESList=c("..","..","..","..","..","..","..","..")
+length(SESList)
 
+PartnerType =c("MArried","Separated","Divorced","Widowed","Never Married")
